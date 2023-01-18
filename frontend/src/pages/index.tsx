64ecac326e677b56b18d0ff3e44c9a29b48efb52
mdrgoncalves <mdr.rgon@gmail.com>
2023-01-18 12:59:24 -0300

    feat: criada página de login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,51 @@
+import Head from "next/head"
+import Image from "next/image"
+import styles from "/styles/home.module.scss";
+
+import logoImg from "assets/logo.svg";
+
+import { Input } from "components/ui/Input";
+import Button from "components/ui/Button";
+
+import Link from "next/link";
+
 export default function Home() {
     return (
+        <>
+        <Head>
+            <title>ShelterPizza - Login</title>
+        </Head>
         <div>
-            <h1>Hello World</h1>
+            <div className={styles.containerCenter}>
+                <Image src={logoImg} alt="Logo Shelter Pizza" />
+
+                <div className={styles.login}>
+                    <form>
+                        <Input 
+                            placeholder="Digite seu email"
+                            type="email"
+                        />
+                        <Input
+                            placeholder="Digite sua senha"
+                            type="password"
+                        />
+                        <Button 
+                            type="submit"
+                            loading={false}
+                        >
+                            Acessar
+                        </Button>
+                    </form>
+
+                    <Link 
+                        href="/signup"
+                        className={styles.text}
+                    >
+                        Não possui uma conta? Cadastre-se!
+                    </Link>
+                </div>
+            </div>
         </div>
+        </>
     )
 }
