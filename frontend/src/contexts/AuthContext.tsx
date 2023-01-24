b0b912a198f0f5ec60fb61e4093b631911d8555c
mdrgoncalves <mdr.rgon@gmail.com>
2023-01-24 15:23:40 -0300

    feat: aplicado ciclo de vida de usuario nas paginas ao authcontext

--- frontend/src/contexts/AuthContext.tsx
+++ frontend/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState, useContext } from "react";
+import { createContext, ReactNode, useState, useContext, useEffect } from "react";
 
 import { api } from "services/apiClient";
 
@@ -52,6 +52,29 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
     const [user, setUser] = useState<UserProps>();
     const isAuthenticated = !!user;
+
+    useEffect(() => {
+        
+        // Tentar pegar algo no cookie
+        const { '@nextauth.token': token } = parseCookies();
+
+        if (token) {
+            api.get('/me')
+                .then(response => {
+                    const { id, name, email } = response.data;
+
+                    setUser({
+                        id,
+                        name,
+                        email
+                    });
+                })
+                .catch(() => {
+                    signOut();
+                })
+        }
+
+    }, []);
 
     async function signIn({ email, password }: SignInProps) {
         try {
